c81f6885cc600d56e50a4de8efa995a0c17c54a1
Uday <uday@Itsupports-MacBook-Pro.local>
2019-11-18 10:33:29 +0530

    initial commit

---
+++ HospitalManagement/Views/MasterPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HospitalManagement.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+        MasterDetailPage mainPage;
+        public MasterPage()
+        {
+            InitializeComponent();
+
+            SetupMenu();            
+        }
+
+        public void SetupMenu()
+        {
+            var menuList = new List<MenuItem>
+            {
+                new MenuItem{ Title = "Patient"},
+                new MenuItem{ Title = "New Treatment"},                
+                new MenuItem{ Title = "Report"},
+                            
+            };
+
+            listView.ItemsSource = menuList;
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            mainPage = Application.Current.MainPage as MasterDetailPage;
+            var menu = e.Item as MenuItem;
+            switch (menu.Title)
+            {
+                case "Patient":
+                    mainPage.Detail = new NavigationPage(new AddPatient());
+                break;
+
+                case "New Treatment":
+                    mainPage.Detail = new NavigationPage(new PatientDetailPage());
+                break;
+                    
+                case "Report":
+                    mainPage.Detail = new NavigationPage(new MainPage());
+                    break;
+            }
+
+            mainPage.IsPresented = false;
+        }
+
+        private void Synch(object sender, EventArgs e)
+        {
+            mainPage = Application.Current.MainPage as MasterDetailPage;
+            mainPage.Detail = new NavigationPage(new SyncData());
+        }
+    }
+}
