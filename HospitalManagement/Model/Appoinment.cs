7b0e8290c32bb10fce424acaa6640427b4292375
Abraham Muthiah <abrahammuthiah@gmail.com>
2019-11-19 08:05:16 +0530

    Added appoinments

---
+++ HospitalManagement/Model/Appoinment.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagement.Model
+{
+    class Appoinment
+    {
+        [AutoIncrement][PrimaryKey]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string TreatmentPlan { get; set; }
+
+        public string TreatmentDone { get; set; }
+    }
+}
