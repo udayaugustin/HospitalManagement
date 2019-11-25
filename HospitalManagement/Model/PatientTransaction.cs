dcfefcd89ff86113448e47a09d21190dd749a51b
Abraham Muthiah <abrahammuthiah@gmail.com>
2019-11-25 14:06:16 +0530

    file added

---
+++ HospitalManagement/Model/PatientTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+
+namespace HospitalManagement.Model
+{
+    public class PatientTransaction
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int PatientId { get; set; }
+
+        public string PatientName { get; set; }
+
+        public int  PaidAmount { get; set; }
+    }
+}
